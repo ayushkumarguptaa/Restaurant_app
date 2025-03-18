77ad7b931bbde80bd2fa686389f63abfa718e5fd
ayush <ayushgupta.ce@gmail.com>
2025-03-18 22:57:16 +0530

    file uploaded

---
+++ src/components/MenuApi.jsx
@@ -0,0 +1,81 @@
+const MenuApi = [
+    {
+        Name : "Maggie",
+        Category : "Breakfast",
+        id : 1,
+        image : "./Images/Maggie.webp",
+        description : "Maggi is an instant noodle brand that's available in restaurants in a variety of styles, including plain, masala, and cheese"
+    },
+    {
+        Name : "Burger",
+        Category : "Breakfast",
+        id : 2,
+        image : "./Images/Burger.jpg",
+        description : "A juicy, flame-grilled, half-pound Angus beef patty piled high with crisp lettuce, fresh tomato, red onion, melted cheddar cheese, and our signature house sauce on a toasted sesame seed bun"
+    },
+    {
+        Name : "Pasta",
+        Category : "Breakfast",
+        id : 3,
+        image : "./Images/Pasta.avif",
+        description : "Pasta is a food made from flour, water, and sometimes egg, that comes in many shapes and is often served with sauce"
+    },
+    {
+        Name : "Pizza",
+        Category : "Breakfast",
+        id : 4,
+        image : "./Images/Pizza.jpg",
+        description : "When describing pizza for a restaurant, you can mention the ingredients, cooking method, and how it's served. You can also use descriptive words to highlight the quality of the pizza"
+    },
+    {
+        Name : "Akki Roti",
+        Category : "Lunch",
+        id : 5,
+        image : "./Images/akki_roti.jpg",
+        description : "Akki Roti, a soft-n-crispy flatbread made with rice flour, grated carrot, onion and basic Indian spices"
+    },
+    {
+        Name : "Ghee Rice",
+        Category : "Lunch",
+        id : 6,
+        image : "./Images/ghee_rice.jpg",
+        description : "Ghee rice is a popular South Indian dish made with rice, ghee, and spices. It's often prepared in a single pot and can be served with a variety of dishes"
+    },
+    {
+        Name : "Mango Rice",
+        Category : "Lunch",
+        id : 7,
+        image : "./Images/mango_rice.jpeg",
+        description : "Mango rice is a flavorful South Indian dish made with rice, raw mango, and spices. It can be tangy and slightly spicy"
+    },
+    {
+        Name : "Biryani",
+        Category : "Dinner",
+        id : 8,
+        image : "Images/rice and biryani.jpeg",
+        description : " "
+    },
+    {
+        Name : "mix vegetable",
+        Category : "Dinner",
+        id : 9,
+        image : "Images/mix-vegetable.jpeg",
+        description : " "
+    },
+    {
+        Name : "Chana Masala",
+        Category : "Dinner",
+        id : 10,
+        image : "./Images/chana-masala.jpg",
+        description : " "
+    },
+    {
+        Name : "Dhal",
+        Category : "Dinner",
+        id : 11,
+        image : "./Images/dhal.jpeg",
+        description : " "
+    }
+]
+
+export default MenuApi
